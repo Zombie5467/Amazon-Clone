9b769715bef50906c4c8ceeedc22e58517138aeb
Addel <sigmamediacode@gmail.com>
2024-10-08 21:51:06 -0400

    Cambio de Disco Duro

---
+++ data/deliveryOptions.js
@@ -0,0 +1,14 @@
+// step 1 save the data
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: '7',
+    priceCents: 0
+}, {
+    id: '2',
+    deliveryDays: '3',
+    priceCents: 499
+},{
+    id: '3',
+    deliveryDays: '1',
+    priceCents: 999
+}]
